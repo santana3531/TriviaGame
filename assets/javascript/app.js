3ae7b9676d3627c80d95d556440b5090ea439064
Arturo <santana3531@yahoo.com>
2017-05-12 18:30:06 -0400

    I SUCK!

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+// Browser load game.
+$(document).ready(function(){
+
+})
+//Start timer
+		//create timer
+		var twoMinutes = setInterval(myTimer, 60 * 2);
+
+		function myTimer() {
+			document.getElementbyID("start-button").append();
+		}
+
+
+
+
+		// $("#start-button").on("click", function(event);
+
+
+		// function startTimer() {
+		// 		var twoMinutes = 60 * 2;
+		// 		twoMinutes--;
+		// 		displa
+		// }
+
+
+
+
+
+
+//collect answers when time runs out
+
+
+//log answers selected
+
+
+
+//count how many right answers
+
+
+//calculate the score of right answers
+
+
+//give score
+
+
+//reset game.
