229bf7adfe333a5e48ca145e6eb6e1f8384c8326
Arturo <santana3531@yahoo.com>
2017-05-14 10:40:34 -0400

    I don't suck anymore!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,59 @@
 // Browser load game.
 $(document).ready(function(){
 
-})
+
 //Start timer
 		//create timer
-		var twoMinutes = setInterval(myTimer, 60 * 2);
+		var time = 45;
+		var timerInterval;
 
 		function myTimer() {
-			document.getElementbyID("start-button").append();
+			time--;
+			console.log(time);
+			$(".displayTime").html(time);
+		 // if time equals zero do this 
+				if (time === 0) {
+					clearInterval(timerInterval);
+					alert("Times-Up!");
+					displayResults();
+
+				}
+		
 		}
 
+		$("#start-button").on("click", function(event){
+			timerInterval = setInterval(myTimer, 1000);
+
+		});
+
+
+		function displayResults() {
+  			var results = $('input[type=radio]:checked');
+  			for (var i = 0; i < results.length; i++) {
+  				results[i]
+  				console.log(results[i].value);
+
+  			if(results[i].value == 1) {
+  				$(results[i]).addClass("correct")
+
+  			} else {
+  				$(results[i]).addClass("wrong")
+  			}
+  		}
+
+  	}
+ 
+ 		$("#submit").on("click", function(event){
+			clearInterval(timerInterval);
+			displayResults();
+ 		})
 
 
 
-		// $("#start-button").on("click", function(event);
-
-
-		// function startTimer() {
-		// 		var twoMinutes = 60 * 2;
-		// 		twoMinutes--;
-		// 		displa
-		// }
 
 
 
-
-
-
-//collect answers when time runs out
+//collect all the values of 1 as correct answer
 
 
 //log answers selected
@@ -43,4 +69,5 @@ $(document).ready(function(){
 //give score
 
 
+}) // end of document.ready "load"
 //reset game.
